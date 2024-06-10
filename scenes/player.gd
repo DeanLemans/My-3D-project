ca6862f611c36b7100640e20297e5ebaf90b9f7d
Dean <111811264+DeanLemans@users.noreply.github.com>
2024-06-10 11:52:41 +0200

    added img folder, changed texture of objetcs

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+@onready var animation_player = $Node3D/mixamo_base/AnimationPlayer
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -19,6 +20,7 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		animation_player.play("walking")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
