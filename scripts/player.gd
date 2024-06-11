38f074a6d2d3d7c4ec8980717a04de44eb040414
Dean <111811264+DeanLemans@users.noreply.github.com>
2024-06-11 12:41:04 +0200

    added jumpscare and shitty blood particles

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,9 @@ const JUMP_VELOCITY = 4.5
 var walking_speed = 3.0
 var running_speed = 4.0
 
+#func _ready() -> void:
+	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,14 +22,25 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var input_dir := Input.get_vector("right", "left", "down", "up")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		animation_player.play("walking")
+		#animation_player.play("walking")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		#animation_player.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	animation_player.play("knock_down")
+	print("should play knock_down animation")
+
+#todo make animation lock
+
+
+func _on_spooky_body_entered(body: Node3D) -> void:
+	get_tree().change_scene_to_file("res://scenes/jumpscare.tscn")
