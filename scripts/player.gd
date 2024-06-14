6a2a07e513b5394f5bcf139896e6e9779f51aff3
Dean <111811264+DeanLemans@users.noreply.github.com>
2024-06-14 11:02:01 +0200

    update jumpscare scene

--- scripts/player.gd
+++ scripts/player.gd
@@ -48,4 +48,4 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 
 
 func _on_spooky_body_entered(body: Node3D) -> void:
-	get_tree().change_scene_to_file("res://scenes/jumpscare.tscn")
+	get_tree().change_scene_to_file("res://scenes/JumpScarer.tscn")
